d26158c88bb60df27cb4c1906cfd3da711ee75a7
Mohit Arya <mohit@thecodefusion.com>
2021-11-22 15:41:38 +0530

    added consultant component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -94,7 +94,11 @@ export const appRoutes: Route[] = [
             {path: 'shipping', loadChildren: () => import('app/modules/admin/shipping/shipping.module').then(m => m.ShippingModule)},
             {path: 'questions', loadChildren: () => import('app/modules/admin/questions/questions.module').then(m => m.QuestionsModule)},
 <<<<<<< HEAD
+<<<<<<< HEAD
             {path: 'add-Service', loadChildren: () => import('app/modules/admin/add-services/add-services.module').then(m => m.AddServiceModule)},
+=======
+            {path: 'consultant', loadChildren: () => import('app/modules/admin/consultant/consultant.module').then(m => m.ConsultantModule)},
+>>>>>>> c8bf1a5 (added consultant component)
 =======
             {path: 'consultant', loadChildren: () => import('app/modules/admin/consultant/consultant.module').then(m => m.ConsultantModule)},
 >>>>>>> c8bf1a5 (added consultant component)
